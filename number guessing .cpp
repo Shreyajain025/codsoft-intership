7501bc2ae1b53f2bd9cb4ae44a52242e674b1d53
Shreyajain025 <jainshraddha092@gmail.com>
2024-07-23 10:56:19 +0530

    Add files via upload

---
+++ number guessing .cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+
+int range;
+cout<<"enter a range of random number: " ;
+cin>>range;
+int random =rand()% range;
+    cout<<"guess a number!!"<<endl;
+    int userGuess;
+
+    do
+    {
+        cout << " your guess: ";
+        cin >> userGuess;
+         if(userGuess > range){
+            cout <<"the number you guessed is out of range!!"<<endl;
+
+         }
+        else if (userGuess >random)
+        {
+            cout << "Too high!" << endl;
+        }
+        else if (userGuess <random)
+        {
+            cout << "Too low!" << endl;
+        }
+
+    } while (userGuess != random);
+
+    cout << "Congratulations! You guessed the right number." << endl;
+
+    return 0;
+}
